e720081490c2a31c29d57a5e6fddd5177261727d
ChaeSangJung <shtingstar@hanmail.net>
2020-10-15 00:01:53 +0900

    #2.5

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
-const express = require('express');
+import express from "express"
 const app = express();
 
 const PORT = 4000;
 
-function handleListening() {
-  console.log(`Listening on: http://localhost:${PORT}`);
-}
+const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
 
-function handleProfile(req, res) {
-  res.send("You are on my profile");
-}
+const handleProfile = (req, res) => res.send("You are on my profile");
 
-function handleHome(req, res) {
-  res.send("Hello from home");
-  // console.log("Hello from home");
-  // console.log(req);
-}
+const handleHome = (req, res) => res.send("Hello from home");
 
 app.get('/', handleHome);
 
