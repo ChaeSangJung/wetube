9ab0c4c52ea0ef0745b015205ef42c026c08f1b0
ChaeSangJung <shtingstar@hanmail.net>
2020-10-14 23:22:50 +0900

    #2.4

--- index.js
+++ index.js
@@ -4,7 +4,21 @@ const app = express();
 const PORT = 4000;
 
 function handleListening() {
-    console.log(`Listening on: http://localhost:${PORT}`);
-  }
-  
+  console.log(`Listening on: http://localhost:${PORT}`);
+}
+
+function handleProfile(req, res) {
+  res.send("You are on my profile");
+}
+
+function handleHome(req, res) {
+  res.send("Hello from home");
+  // console.log("Hello from home");
+  // console.log(req);
+}
+
+app.get('/', handleHome);
+
+app.get("/profile", handleProfile);
+
 app.listen(PORT, handleListening);
